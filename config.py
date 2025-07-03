94e1df4f0ac72084e087e53540970f21ea81ac94
nepa27 <nepalek@mail.ru>
2025-07-03 11:13:03 +0300

    Change ttl of token

--- config.py
+++ config.py
@@ -97,7 +97,7 @@ def refresh_expiring_jwts(response):
         if g.user:
             exp_timestamp = get_jwt()['exp']
             now = datetime.now(timezone.utc)
-            target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+            target_timestamp = datetime.timestamp(now + timedelta(hours=3600))
 
             if target_timestamp > exp_timestamp:
                 access_token = create_access_token(identity=get_jwt_identity())
